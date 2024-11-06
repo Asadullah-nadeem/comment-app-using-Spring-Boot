e1f9ead221b0be2fbe222a72a6d105f6e6469efc
Muhammad Asadullah Nadeem <88024587+Asadullah-nadeem@users.noreply.github.com>
2024-11-06 21:39:32 +0530

    Add code Demo Version

---
+++ src/test/java/example/com/comment_app/CommentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package example.com.comment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
